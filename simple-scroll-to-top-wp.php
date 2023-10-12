f1f3d161f63d90abdfffed69cd234d3e1866d9b3
Arman Hossain <armanhossaincse7@gmail.com>
2023-10-12 18:46:29 +0600

    plugin php file info

---
+++ simple-scroll-to-top-wp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Simple Scroll To Top WP
+ * Plugin URI: https://wordpress.org/plugins/simple-scroll-to-top-wp
+ * Description: Simple scroll to top plugin will help you.
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.4
+ * Author: Arman
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI: https://github.com/armancse-dev
+ * Text Domain: sstt
+ */
+?>
