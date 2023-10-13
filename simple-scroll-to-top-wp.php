18abac1ee7473bec7d09611bd80ffd7c4484ec63
Arman Hossain <armanhossaincse7@gmail.com>
2023-10-13 19:37:14 +0600

    plugin file management

--- simple-scroll-to-top-wp.php
+++ simple-scroll-to-top-wp.php
@@ -12,4 +12,30 @@
  * Update URI: https://github.com/armancse-dev
  * Text Domain: sstt
  */
+
+   // Including CSS
+  function sstt_enqueue_style(){
+    wp_enqueue_style('sstt-style', plugins_url('css/themes/sstt-style.css', __FILE__));
+  }
+  add_action( "wp_enqueue_scripts", "sstt_enqueue_style" );
+
+
+  //includeingJS
+  function sstt_enqueue_script(){
+      wp_enqueue_script('jquery');
+      wp_enqueue_script('sstt-plugin-script', plugins_url('js/sstt-plugin.js', __FILE__), array(), '1.0.0', true);
+  }
+
+  add_action( "wp_enqueue_scripts", "sstt_enqueue_script" );
+
+    // jQuery Plugin Setting Activation
+    function sstt_scroll_script(){?>
+      <script>
+      jQuery(document).ready(function () {
+        jQuery.scrollUp();
+      });
+      </script>
+      <?php }
+      add_action( "wp_footer", "sstt_scroll_script" );
+      
 ?>
